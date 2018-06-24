ca6f8d3b37ca95b26c7d11ff0a3fba4ba80c6214
rajesh <rajeshsingh.it90@gmail.com>
2018-06-24 16:59:20 +0530

    First Change

--- TestBasicJavaProject/src/main/java/com/main/TestMainClass.java
+++ TestBasicJavaProject/src/main/java/com/main/TestMainClass.java
@@ -3,8 +3,9 @@ package com.main;
 public class TestMainClass {
 
 	public static void main(String[] args) {
+		int num = 2+3;
 		System.out.println("Hello Friend");
-
+		System.out.println("Addition :"+num);
 	}
 
 }
